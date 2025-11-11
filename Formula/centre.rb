880c3948535478dfd38a426545d5b6bc17e08a0f
Jose Medrano <jmedranocordoba@gmail.com>
2025-11-11 18:29:00 +0100

    Removes x86 support

--- Formula/centre.rb
+++ Formula/centre.rb
@@ -4,15 +4,8 @@ class Centre < Formula
   version "1.0.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/josepmed/centre/releases/download/v1.0.0/centre-1.0.0-arm64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA"
-    else
-      url "https://github.com/josepmed/centre/releases/download/v1.0.0/centre-1.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_SHA"
-    end
-  end
+  url "https://github.com/josepmed/centre/releases/download/v1.0.0/centre-1.0.0-arm64-apple-darwin.tar.gz"
+  sha256 "PLACEHOLDER_ARM64_SHA"
 
   def install
     bin.install "centre"
